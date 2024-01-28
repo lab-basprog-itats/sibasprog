12cdaf295074cbe0cda92aeadbff8fd819d52018
lab-basprog-itats <laboratorium.basprog@gmail.com>
2024-01-28 22:41:44 +0700

    initial commit

---
+++ app/Http/Controllers/AslabController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aslab;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AslabController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Inertia\Response
+    {
+        return Inertia::render('Test');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aslab $aslab)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aslab $aslab)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Aslab $aslab)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aslab $aslab)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,10 @@
+const Test = () => {
+    return (
+        <>
+            <h1 className="text-center text-red-500 font-bold text-sm">
+                HELLO
+            </h1>
+        </>
+    )
+};
+export default Test;
