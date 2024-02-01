d4807b54b56370d76878675030c4921dcbfad30a
lab-basprog-itats <laboratorium.basprog@gmail.com>
2024-02-02 04:45:27 +0700

    Setup Autentikasi Admin,Aslab dan Praktikan

--- app/Models/Aslab.php
+++ app/Models/Aslab.php
@@ -3,9 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class Aslab extends Model
+class Aslab extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+
+    protected $table = 'aslab';
+    protected $primaryKey = 'id';
+    protected $guarded = ['id'];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
