9e78e0bf58f13740f6ee2e47270ece97c9f4b472
Mahameruuu <114761614+Mahameruuu@users.noreply.github.com>
2024-02-06 22:54:13 +0700

    Update ListPraktikan.jsx

--- resources/js/views/ListPraktikan.jsx
+++ resources/js/views/ListPraktikan.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const ListPraktikan = () => {
-  // Logika untuk menampilkan list praktikan
   return (
     <div>
       <h1>List Praktikan</h1>
-      {/* Tambahkan komponen untuk menampilkan list praktikan */}
+      {}
     </div>
   );
 };
