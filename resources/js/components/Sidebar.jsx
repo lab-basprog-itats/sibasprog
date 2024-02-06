17b8657a67c8aaa7ccfbfee2a2a97ea9a509a0d0
Ryan <ryanrga01@gmail.com>
2024-02-06 23:21:57 +0700

    Fixing Program

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
-import { CubeTransparentIcon } from '@heroicons/react/24/solid'; 
+import React, { useState } from "react";
+import { CubeTransparentIcon } from "@heroicons/react/24/solid";
 
 const Sidebar = ({ menuOptions }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
 
-  return (
-    <aside className={`bg-white text-blue-800 ${isSidebarOpen ? 'w-64' : 'w-16'} p-8 transition-all duration-300 ease-in-out relative`}>
-      <div className="flex items-center mb-8">
-        <CubeTransparentIcon className="h-8 w-8 mr-2" />
-        <h2 className="text-lg font-bold">ASLAB PAGE</h2>
-      </div>
-      <ul>
-        {menuOptions.map((option, index) => (
-          <li key={index} className="py-2 flex items-center">
-            <CubeTransparentIcon className="h-5 w-5 mr-2" />
-            <a href="#">{option}</a>
-          </li>
-        ))}
-      </ul>
-      <button className="absolute bottom-4 left-4 text-blue-700" onClick={toggleSidebar}>
-        {isSidebarOpen ? 'Close' : 'Open'}
-      </button>
-    </aside>
-  );
+    return (
+        <aside
+            className={`bg-white text-blue-800 ${
+                isSidebarOpen ? "w-64" : "w-16"
+            } p-8 transition-all duration-300 ease-in-out relative`}
+        >
+            <div className="flex items-center mb-8">
+                <CubeTransparentIcon className="h-8 w-8 mr-2" />
+                <h2 className="text-lg font-bold">ASLAB PAGE</h2>
+            </div>
+            <ul>
+                {menuOptions.map((option, index) => (
+                    <li key={index} className="py-2 flex items-center">
+                        <CubeTransparentIcon className="h-5 w-5 mr-2" />
+                        <a href="#">{option}</a>
+                    </li>
+                ))}
+            </ul>
+            <button
+                className="absolute bottom-4 left-4 text-blue-700"
+                onClick={toggleSidebar}
+            >
+                {isSidebarOpen ? "Close" : "Open"}
+            </button>
+        </aside>
+    );
 };
 
 export default Sidebar;
--- resources/js/views/Dashboard.jsx
+++ resources/js/views/Dashboard.jsx
@@ -1,53 +1,61 @@
-import React from 'react';
-import { CubeTransparentIcon } from '@heroicons/react/24/solid';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar'; 
+import React from "react";
+import { CubeTransparentIcon } from "@heroicons/react/24/solid";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
-  const adminMenuOptions = ['Dashboard', 'List Praktikan', 'Entry Nilai', 'Entry Pelanggaran', 'Unduh Nilai']; 
+    const adminMenuOptions = [
+        "Dashboard",
+        "List Praktikan",
+        "Entry Nilai",
+        "Entry Pelanggaran",
+        "Unduh Nilai",
+    ];
 
-  return (
-    <div className="flex flex-col md:flex-row"> 
-      <Sidebar menuOptions={adminMenuOptions} />
-      <div className="flex-grow text-blue-700">
-        <Header> 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"> 
-            <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
-              <CubeTransparentIcon className="h-6 w-6" />
-              <div>
-                <p className='text-3xl size-10'>10</p>
-                <h3 className="text-xl mb-2">Total Praktikan</h3>
-              </div>
-            </div>
+    return (
+        <div className="flex flex-col md:flex-row">
+            <Sidebar menuOptions={adminMenuOptions} />
+            <div className="flex-grow text-blue-700">
+                <Header>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">10</p>
+                                <h3 className="text-xl mb-2">
+                                    Total Praktikan
+                                </h3>
+                            </div>
+                        </div>
 
-            <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
-              <CubeTransparentIcon className="h-6 w-6" />
-              <div>
-                <p className='text-3xl size-10'>5</p>
-                <h3 className="text-xl mb-2">Total Aslab</h3>
-              </div>
-            </div>
+                        <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">5</p>
+                                <h3 className="text-xl mb-2">Total Aslab</h3>
+                            </div>
+                        </div>
 
-            <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
-              <CubeTransparentIcon className="h-6 w-6" />
-              <div>
-                <p className='text-3xl size-10'>4</p>
-                <h3 className="text-xl mb-2">Total Tugas</h3>
-              </div>
-            </div>
+                        <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">4</p>
+                                <h3 className="text-xl mb-2">Total Tugas</h3>
+                            </div>
+                        </div>
 
-            <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
-              <CubeTransparentIcon className="h-6 w-6" />
-              <div>
-                <p className='text-3xl size-10'>0</p>
-                <h3 className="text-xl mb-2">Total Other</h3>
-              </div>
+                        <div className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4">
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">0</p>
+                                <h3 className="text-xl mb-2">Total Other</h3>
+                            </div>
+                        </div>
+                    </div>
+                </Header>
             </div>
-          </div>
-        </Header>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Dashboard;
