17ecb0324ca3191361228135ffdbfc9d104bbdf9
Ryan <ryanrga01@gmail.com>
2024-02-07 09:20:30 +0700

    Fixing Program

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,4 +1,23 @@
+import React from "react";
+import { InertiaLink } from "@inertiajs/inertia-react";
 
+const Header = ({ children }) => {
+    return (
+        <div className="bg-blue-100 min-h-screen flex-col">
+            <div className="flex items-center justify-between px-4 sm:px-10 py-1 bg-blue-900 shadow-md">
+                <div className="h-12 sm:h-28 ml-auto">
+                    {/* Opsi profil */}
+                    <InertiaLink
+                        href="/profile"
+                        className="text-white hover:text-blue-100 h-12 sm:h-auto"
+                    >
+                        Profil
+                    </InertiaLink>
+                </div>
+            </div>
+            <div className="w-full max-w-6xl mx-auto mt-[-50px] h-full p-4 sm:p-12 bg-gray-100 rounded-lg shadow-lg">
+                {children}
+            </div>
         </div>
     );
 };
--- resources/js/views/Dashboard.jsx
+++ resources/js/views/Dashboard.jsx
@@ -1,4 +1,8 @@
-
+import React from "react";
+import { CubeTransparentIcon } from "@heroicons/react/24/solid";
+import { InertiaLink } from "@inertiajs/inertia-react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
     const adminMenuOptions = [
@@ -9,7 +13,62 @@ const Dashboard = () => {
         "Unduh Nilai",
     ];
 
+    return (
+        <div className="flex flex-col md:flex-row">
+            <Sidebar menuOptions={adminMenuOptions} />
+            <div className="flex-grow text-blue-700">
+                <Header>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <InertiaLink
+                            href="/dashboard"
+                            className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4"
+                        >
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">10</p>
+                                <h3 className="text-xl mb-2">
+                                    Total Praktikan
+                                </h3>
+                            </div>
+                        </InertiaLink>
 
+                        <InertiaLink
+                            href="/aslabs"
+                            className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4"
+                        >
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">5</p>
+                                <h3 className="text-xl mb-2">Total Aslab</h3>
+                            </div>
+                        </InertiaLink>
+
+                        <InertiaLink
+                            href="/tasks"
+                            className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4"
+                        >
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">4</p>
+                                <h3 className="text-xl mb-2">Total Tugas</h3>
+                            </div>
+                        </InertiaLink>
+
+                        <InertiaLink
+                            href="/others"
+                            className="bg-white rounded-md shadow-md p-6 flex items-center space-x-4"
+                        >
+                            <CubeTransparentIcon className="h-6 w-6" />
+                            <div>
+                                <p className="text-3xl size-10">0</p>
+                                <h3 className="text-xl mb-2">Total Other</h3>
+                            </div>
+                        </InertiaLink>
+                    </div>
+                </Header>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
