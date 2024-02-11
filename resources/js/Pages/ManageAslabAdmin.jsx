afed8c30c01af8f0a5ae8a7ef3058d790c3b3f7c
Ryan <ryanrga01@gmail.com>
2024-02-11 12:26:37 +0700

    Update Memanggil components

---
+++ resources/js/Pages/ManageAslabAdmin.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { InertiaLink } from "@inertiajs/inertia-react";
+import HeaderAslab from "../components/HeaderAdmin";
+import FooterAslab from "../components/FooterAdmin";
+import SidebarAslab from "../components/SidebarAdmin";
