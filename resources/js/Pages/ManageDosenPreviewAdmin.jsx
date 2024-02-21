4a36c7a5636b97a9cb6b5f7ba51daf0aadc25db1
Ryan <ryanrga01@gmail.com>
2024-02-21 08:57:50 +0700

    Updating New Admin

--- resources/js/Pages/ManageDosenPreviewAdmin.jsx
+++ resources/js/Pages/ManageDosenPreviewAdmin.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { InertiaLink } from "@inertiajs/inertia-react";
-import HeaderAdmin from "../components/HeaderAdmin";
-import FooterAdmin from "../components/FooterAdmin";
-import SidebarAdmin from "../components/SidebarAdmin";
-import MenuAdmin from "../components/MenuAdmin";
-// import DataStaticAdmin from "../components/DataStaticAdmin";
+import HeaderAdmin from "../Components/HeaderAdmin";
+import FooterAdmin from "../Components/FooterAdmin";
+import SidebarAdmin from "../Components/SidebarAdmin";
+import MenuAdmin from "../Components/MenuAdmin";
+// import DataStaticAdmin from "../Components/DataStaticAdmin";
 
 const ManageDosenPreviewAdmin = () => {
     const data = [
@@ -61,73 +61,6 @@ const ManageDosenPreviewAdmin = () => {
     ];
 
     return (
-        //         <div>
-        //             <HeaderAdmin />
-        //             <div className="flex">
-        //                 <SidebarAdmin menuOptions={adminMenuOptions} />
-        //                 <div className="flex-1 bg-gray-100 min-h-screen">
-        //                     <div className="p-8">
-        //                         <h1 className="text-2xl font-semibold mb-4">
-        //                             Preview Dosen
-        //                         </h1>
-        //                         <InertiaLink
-        //                             href="/register-dosen"
-        //                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        //                         >
-        //                             Register Dosen
-        //                         </InertiaLink>
-        //                     </div>
-        //                     <div className="bg-white shadow-md rounded-lg overflow-hidden">
-        //                         <table className="min-w-full divide-y divide-gray-200">
-        //                             <thead className="bg-gray-50">
-        //                                 <tr>
-        //                                     <th
-        //                                         scope="col"
-        //                                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-        //                                     >
-        //                                         NIP
-        //                                     </th>
-        //                                     <th
-        //                                         scope="col"
-        //                                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-        //                                     >
-        //                                         Nama Dosen
-        //                                     </th>
-        //                                     <th
-        //                                         scope="col"
-        //                                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-        //                                     >
-        //                                         No Telepon
-        //                                     </th>
-        //                                 </tr>
-        //                             </thead>
-        //                             <tbody className="bg-white divide-y divide-gray-200">
-        //                                 {data.map((dosen, idx) => (
-        //                                     <tr key={idx}>
-        //                                         <td className="px-6 py-4 whitespace-nowrap">
-        //                                             <div className="text-sm text-gray-900">
-        //                                                 {dosen.nip}
-        //                                             </div>
-        //                                         </td>
-        //                                         <td className="px-6 py-4 whitespace-nowrap">
-        //                                             <div className="text-sm text-gray-900">
-        //                                                 {dosen.name}
-        //                                             </div>{" "}
-        //                                         </td>{" "}
-        //                                         <td className="px-6 py-4 whitespace-nowrap">
-        //                                             {" "}
-        //                                             <div className="text-sm text-gray-900">
-        //                                                 {dosen.phoneNumber}
-        //                                             </div>{" "}
-        //                                         </td>{" "}
-        //                                     </tr>
-        //                                 ))}{" "}
-        //                             </tbody>{" "}
-        //                         </table>{" "}
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
         <div className="flex flex-col md:flex-row">
             <SidebarAdmin menuOptions={MenuAdmin.adminMenuOptions} />
             <div className="flex-grow text-blue-700">
