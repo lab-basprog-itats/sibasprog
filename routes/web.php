3c66d1671239f5dbefea5b11c338c5f49738e5c4
CONTOH-DEV <062021107454@mhs.itats.ac.id>
2024-02-22 20:17:08 +0700

    update package,  package.lock & web.php (routers)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\AslabController;
+// use App\Http\Controllers\AslabController;
 use App\Models\Aslab;
-use Illuminate\Routing\Router;
+// use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
